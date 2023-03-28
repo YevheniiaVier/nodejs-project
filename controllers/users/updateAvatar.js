aea1e2fc31972e200c0e115b0b21833e2d6adf90
YevheniiaVier <gotashk1983@gmail.com>
2023-03-28 19:41:13 +0300

    adds jimp resize

--- controllers/users/updateAvatar.js
+++ controllers/users/updateAvatar.js
@@ -1,10 +1,12 @@
 const path = require("path");
 const fs = require("fs/promises");
-
+const Jimp = require("jimp");
 const { User } = require("../../models");
 const { RequestError } = require("../../errorHandlers/");
 
 const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
+
+const AVATAR_SIZE = 250;
 
 const updateAvatar = async (req, res) => {
   const { path: tempUpload, originalname } = req.file;
@@ -13,6 +15,10 @@ const updateAvatar = async (req, res) => {
   try {
     const resultUpload = path.join(avatarsDir, avatarName);
     await fs.rename(tempUpload, resultUpload);
+
+    const image = await Jimp.read(resultUpload);
+    await image.resize(AVATAR_SIZE, AVATAR_SIZE).write(resultUpload);
+
     const avatarURL = path.join("public", "avatars", avatarName);
 
     await User.findByIdAndUpdate(req.user._id, { avatarURL });
